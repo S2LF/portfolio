10bdf0a49b64e950ca6e725e9471cb7cf2916963
S2LF <sdeuhelf@gmail.com>
2021-09-21 13:49:53 +0200

    update asset root

--- src/index.php
+++ src/index.php
@@ -84,14 +84,14 @@ if(isset($_POST['post'])){
     <meta property="og:url" content="https://www.sylvainallain.fr/">
     <meta property="og:title" content="Sylvain ALLAIN - Développeur web">
     <meta property="og:description" content="Site de présentation d'un dévelopeur web & web mobile">
-    <meta property="og:image" content=".assets/img/MonSite.png">
+    <meta property="og:image" content="assets/img/MonSite.png">
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://www.sylvainallain.fr/">
     <meta property="twitter:title" content="Sylvain ALLAIN - Développeur web">
     <meta property="twitter:description" content="Site de présentation d'un dévelopeur web & web mobile">
-    <meta property="twitter:image" content=".assets/img/MonSite.png">
+    <meta property="twitter:image" content="assets/img/MonSite.png">
 
     <link rel="icon" href=".favicon.ico">
 
@@ -105,9 +105,9 @@ if(isset($_POST['post'])){
     <script src="https://kit.fontawesome.com/64cfa211f1.js" crossorigin="anonymous"></script>
 
 
-<link rel="stylesheet" href=".assets/css/timeline.css">
-    <link rel="stylesheet" href=".assets/css/style.css">
-    <link rel="stylesheet" href=".assets/css/responsive.css">
+<link rel="stylesheet" href="assets/css/timeline.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-146719243-1"></script>
@@ -147,7 +147,7 @@ if(isset($_POST['post'])){
 <header class="change">
     <div class="flex">
 
-            <a href="#"><img src=".assets/img/LogoASPixel4.png" alt="Mon logo"></a>
+            <a href="#"><img src="assets/img/LogoASPixel4.png" alt="Mon logo"></a>
 
         <section>
             <h1>Sylvain ALLAIN - Développeur web</h1>
@@ -176,7 +176,7 @@ if(isset($_POST['post'])){
         <main>
         <article class="intro">
                 <figure class="center">
-                    <img src=".assets/img/moi-removebg.png" alt="Avatar South Park me représentant">
+                    <img src="assets/img/moi-removebg.png" alt="Avatar South Park me représentant">
                 </figure>
                 <h2 class="not-blue">Moi c’est Sylvain,</h2>
                 <h3 class="center not-blue">
@@ -184,7 +184,7 @@ if(isset($_POST['post'])){
                     <strong>Je suis Développeur Web !</strong>
                 </h3>
 
-               <p class="center"><a class="cv" href=".assets/docs/CV-SylvainALLAIN.pdf" target="blank">TELECHARGEZ MON CV</a></p>
+               <p class="center"><a class="cv" href="assets/docs/CV-SylvainALLAIN.pdf" target="blank">TELECHARGEZ MON CV</a></p>
 
             </article>
             <hr class="style-two">
@@ -213,7 +213,7 @@ if(isset($_POST['post'])){
                         <div class="center">
                             <a href="https://sylvainallain.fr/" target="_blank">
                             <figure>
-                                <img class="projets-img monsite" src=".assets/img/MonSite.png" alt="image de mon portfolio">
+                                <img class="projets-img monsite" src="assets/img/MonSite.png" alt="image de mon portfolio">
                             </figure>
                         </a>
                             <div class="center">
@@ -234,7 +234,7 @@ if(isset($_POST['post'])){
                             </div>
 
                             <div class="projets-logo">
-                                <img class="logoResize" src=".assets/img/Tech.png" alt="logo langages">
+                                <img class="logoResize" src="assets/img/Tech.png" alt="logo langages">
                             </div>
                         </div>
                 </article>
@@ -244,7 +244,7 @@ if(isset($_POST['post'])){
                         <h3 class="center not-blue">Application mobile "Il était un film"</h3>
                         <div class="center">
                             <a href="https://play.google.com/store/apps/details?id=s2lf.cinema" target="_blank">
-                                <img class="projets-img" src=".assets/img/app-mobile.jpg" alt="image de l'application mobile">
+                                <img class="projets-img" src="assets/img/app-mobile.jpg" alt="image de l'application mobile">
                             </a>
                             <div class="center">
                                 <a href="https://play.google.com/store/apps/details?id=s2lf.cinema" target="_blank">
@@ -262,7 +262,7 @@ if(isset($_POST['post'])){
                             </ul>
                         </p>
                         <div  class="projets-logo">
-                            <img class="logoResize" src=".assets/img/stack-mobile.png" alt="logo react native"><br>
+                            <img class="logoResize" src="assets/img/stack-mobile.png" alt="logo react native"><br>
 <!--                             
                             <img class="logoResize"  src="assets/img/expo.jpg" alt="logo langages back"><br> -->
                             <!-- <img class="logoSize" src="assets/img/logo-wordpress.png" alt="logo WordPress"> -->
@@ -272,7 +272,7 @@ if(isset($_POST['post'])){
                         <h3 class="center not-blue">Memory & jeu du Pendu</h3>
                         <div class="center">
                             <a href="https://syl20-projects.herokuapp.com" target="_blank">
-                                <img class="projets-img " src=".assets/img/react-projects.PNG" alt="imge du site Syl20 react projects">
+                                <img class="projets-img " src="assets/img/react-projects.PNG" alt="imge du site Syl20 react projects">
                             </a>
                             <div class="center">
                                 <a href="https://syl20-projects.herokuapp.com" target="_blank"><i class="fas fa-eye fa-3x"></i></a>&nbsp;&nbsp;
@@ -287,7 +287,7 @@ if(isset($_POST['post'])){
                             </ul>
                         </p>
                         <div class="projets-logo">
-                            <img class="" src=".assets/img/mern.png" alt="logo stack mern"><br>
+                            <img class="" src="assets/img/mern.png" alt="logo stack mern"><br>
                             
                         </div>
                     </article>
@@ -298,7 +298,7 @@ if(isset($_POST['post'])){
                         <h3 class="center not-blue">Tof'Box</h3>
                         <div class="center">
                             <a href="https://tofbox.sylvainallain.fr/" target="_blank">
-                                <img class="projets-img " src=".assets/img/tofbox.png" alt="imge du site tof'box">
+                                <img class="projets-img " src="assets/img/tofbox.png" alt="imge du site tof'box">
                             </a>
                             <div class="center">
                                 <a href="https://tofbox.sylvainallain.fr/" target="_blank"><i class="fas fa-eye fa-3x"></i></a>&nbsp;&nbsp;
@@ -313,16 +313,16 @@ if(isset($_POST['post'])){
                             </ul>
                         </p>
                         <div class="projets-logo">
-                            <img class="logoResize" src=".assets/img/Front.png" alt="logo langages front"><br>
-                            <img class="logoResize" src=".assets/img/Back.png" alt="logo langages back"><br>
-                            <img class="logoSize" src=".assets/img/Symfony5.png" alt="logo Symfony5">
+                            <img class="logoResize" src="assets/img/Front.png" alt="logo langages front"><br>
+                            <img class="logoResize" src="assets/img/Back.png" alt="logo langages back"><br>
+                            <img class="logoSize" src="assets/img/Symfony5.png" alt="logo Symfony5">
                         </div>
                     </article>
                     <article class="transition">
                         <h3 class="center not-blue">Joel ALLAIN Photos</h3>
                         <div class="center">
                             <a href="https://joelallainphotos.fr/" target="_blank">
-                                <img id="jap-position" class="projets-img" src=".assets/img/joelallainphotos.png" alt="image du site Joel ALLAIN Photo">
+                                <img id="jap-position" class="projets-img" src="assets/img/joelallainphotos.png" alt="image du site Joel ALLAIN Photo">
                             </a>
                             <div class="center">
                                 <a href="https://joelallainphotos.fr/" target="_blank">
@@ -338,9 +338,9 @@ if(isset($_POST['post'])){
                             </ul>
                         </p>
                         <div  class="projets-logo">
-                            <img class="logoResize" src=".assets/img/Front.png" alt="logo langages front"><br>
-                            <img class="logoResize"  src=".assets/img/Back.png" alt="logo langages back"><br>
-                            <img class="logoSize" src=".assets/img/logo-wordpress.png" alt="logo WordPress">
+                            <img class="logoResize" src="assets/img/Front.png" alt="logo langages front"><br>
+                            <img class="logoResize"  src="assets/img/Back.png" alt="logo langages back"><br>
+                            <img class="logoSize" src="assets/img/logo-wordpress.png" alt="logo WordPress">
                         </div>
                     </article>
                 </div>
@@ -369,7 +369,7 @@ if(isset($_POST['post'])){
                             <h3>Titre professionnel Concepteur développeur d'application web</h3>
                             <span><a href="https://www.wildcodeschool.com/fr-FR" class="link" target="blank">Wild Code School</a> | Obtenu en Novembre 2021</span>
                             <div>
-                                <a href="https://www.wildcodeschool.com/fr-FR" target="blank"><img class="img-float-left" src=".assets/img/logo-wild.png" alt="logo Wild Code School"></a>
+                                <a href="https://www.wildcodeschool.com/fr-FR" target="blank"><img class="img-float-left" src="assets/img/logo-wild.png" alt="logo Wild Code School"></a>
                                 <p>La Wild Code School est <strong>une école innovante</strong>  et un réseau européen de campus qui forment aux métiers tech des spécialistes adaptables.
                                 C'est en alternance avec OCI que c'est déroulé cette formation à distance.&lrm;<br><br>
                                 Durant cette formation j'ai pu approfondir mes connaissances en développement web avec la découverte de nouveaux langages (NodeJs & React). Le contenu de la formation 
@@ -395,7 +395,7 @@ if(isset($_POST['post'])){
                             <h3>Titre professionnel Développeur web & web mobile</h3>
                             <span><a href="https://elan-formation.eu/" class="link" target="blank">ELAN Formation</a> | Obtenu en Juin 2020</span>
                             <div>
-                                <a href="https://elan-formation.eu/" target="blank"><img class="img-float-right" src=".assets/img/logo-elan.png" alt="logo Elan formation"></a>
+                                <a href="https://elan-formation.eu/" target="blank"><img class="img-float-right" src="assets/img/logo-elan.png" alt="logo Elan formation"></a>
                                 <p><strong>Centre de formation</strong> installé en Alsace depuis plus de 25 ans, c'est dans le centre de Strasbourg que j'ai effectué ma formation au <strong>Titre professionnel de Développeur web et web mobile</strong>.&lrm;<br><br>
                                 Formé aux langages front et back-end par des formateurs compétents et passionnés, ils m'ont transmis les bonnes pratiques du développement web et m'ont accompagné durant mes apprentissages du développement, en formation.&lrm;</p>
                             </div>
@@ -407,7 +407,7 @@ if(isset($_POST['post'])){
                             <h3>Stage développeur web Magento 2</h3>
                             <span><a href="https://black.bird.eu/fr/" class="link" target="blank">Blackbird Agency</a> | Mars - Mai 2020</span>
                             <div>
-                                <a href="https://black.bird.eu/fr/" class="link"><img class="img-float-left" src=".assets/img/logo-blackbird.png" alt="logo Blackbird agency"></a>
+                                <a href="https://black.bird.eu/fr/" class="link"><img class="img-float-left" src="assets/img/logo-blackbird.png" alt="logo Blackbird agency"></a>
                                 <p><strong>Agence de développement web</strong> spécialisée dans les projets e-commerce sur la solution CMS Magento.<br><br>
                                 Ce stage m'a permis de découvrir le CMS <strong>Magento 2</strong> et de réaliser mon projet de stage:<br>
                                 Le développement d'un module permettant l’import de fichiers CSV, le traitement et le stockage en base de données de ses informations et enfin l’affichage et la possibilité d’agir sur ces données dans l’espace administrateur de Magento 2. <br><br>
@@ -422,7 +422,7 @@ if(isset($_POST['post'])){
                         <h3>Stage développeur web</h3>
                         <span><a class="link" href="https://www.aw-innovate.com/" target="blank">AW Innovate</a> | Octobre - Novembre 2019</span>
                         <div>
-                        <a href="https://www.aw-innovate.com/" target="blank"><img class="img-float-right" src=".assets/img/logoAW.jpg" alt="logo AW Innovate"></a>
+                        <a href="https://www.aw-innovate.com/" target="blank"><img class="img-float-right" src="assets/img/logoAW.jpg" alt="logo AW Innovate"></a>
                         <p><strong>AW Innovate est une agence digitale conseil en innovation</strong>. Elle accompagne les entreprises qui le souhaitent vers la création de leur site web.&lrm;<br><br>
 
                             Durant 2 semaines, au sein d'une équipe de développeurs, j'ai eu l'occasion de démarrer un projet concret et de réaliser la partie front et back d'un site web.&lrm; <br><br>
@@ -439,7 +439,7 @@ if(isset($_POST['post'])){
                         <h3>Stage développeur web en programmatique</h3>
                         <span><a class="link" href="https://tradelab.com/" target="blank">Tradelab</a> | Mars - Avril 2019</span>
                         <div>
-                            <a href="https://www.tradelab.com" target="blank"><img class="img-float-left" src=".assets/img/TRADELAB_Logo1.png" alt="Logo Tradelab"></a>
+                            <a href="https://www.tradelab.com" target="blank"><img class="img-float-left" src="assets/img/TRADELAB_Logo1.png" alt="Logo Tradelab"></a>
                             <p><strong>Tradelab est une start-up spécialiste des campagnes display, mobiles et vidéos programmatiques.</strong><br><br>
                                 Durant ces 2 semaines, j’ai rencontré des développeurs Front (JavaScript, TypeScript et Angular), Back (PHP), QA (testeurs), Infra (maintenance serveurs).
                                 Cette expérience en milieu professionnel a confirmé mon projet de réorientation dans le milieu du développement informatique. <br><br>
@@ -454,9 +454,9 @@ if(isset($_POST['post'])){
         <section id="contact" class="anchor">
             <h2 >Réseaux & Contacts</h2>
             <div class="center">
-                <div><a class="socials" href=".assets/docs/CV-SylvainALLAIN.pdf" target="blank"><img src=".assets/img/file-pdf-solid.svg" alt="logo PDF"><p>&nbsp; Mon CV</p></a></div>
-                <div><a class="socials" href="https://www.linkedin.com/in/sylvain-allain/" target="blank"><img src=".assets/img/linkedin-brands.svg" alt="logo LinkedIn"><p>&nbsp;LinkedIn</p></a></div>
-                <div><a class="socials" href="https://github.com/S2LF" target="blank"><img src=".assets/img/github-square-brands.svg" alt="Logo GitHub"><p>&nbsp;GitHub</p></a></div>
+                <div><a class="socials" href="assets/docs/CV-SylvainALLAIN.pdf" target="blank"><img src="assets/img/file-pdf-solid.svg" alt="logo PDF"><p>&nbsp; Mon CV</p></a></div>
+                <div><a class="socials" href="https://www.linkedin.com/in/sylvain-allain/" target="blank"><img src="assets/img/linkedin-brands.svg" alt="logo LinkedIn"><p>&nbsp;LinkedIn</p></a></div>
+                <div><a class="socials" href="https://github.com/S2LF" target="blank"><img src="assets/img/github-square-brands.svg" alt="Logo GitHub"><p>&nbsp;GitHub</p></a></div>
             </div>
         </section>
        <hr class="style-two">
@@ -512,7 +512,7 @@ if(isset($_POST['post'])){
         </div>
     </div>
     <footer>
-            <img src=".assets/img/LogoASPixel4.png" alt="Mon logo">
+            <img src="assets/img/LogoASPixel4.png" alt="Mon logo">
             <p>Merci de votre visite</p>
             <p>Mon Portfolio - Version 6</p>
             <p>Made with <i class="fa fa-heart "></i> by Syl20 - Tous droits réservés - <?php echo date('Y') ?></p>
@@ -521,7 +521,7 @@ if(isset($_POST['post'])){
     </footer>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script src=".assets/js/script.js"></script>
+    <script src="assets/js/script.js"></script>
 
     <script>
     $("#check > input, #check > label").on('click', function(e){
